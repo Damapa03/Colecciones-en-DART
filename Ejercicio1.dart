fa1366bd243ba24347062dd818dde2c18caa831c
Daniel Marin <damapa2003@gmail.com>
2024-11-27 17:13:36 +0100

    Ejercicio1

--- Ejercicio1.dart
+++ Ejercicio1.dart
@@ -2,9 +2,26 @@
 
 void main(){
   var genres = <String>{};
-  var input = stdin.readLineSync()!;
+  var num =1;
+  while(true){
+    print("1. Introducir generos\n2. Ver generos\n3. Salir");
+    var num = int.parse(stdin.readLineSync()!);
+    if(num == 1){
+      print("¿Que genero quiere añadir?");
+      var input = stdin.readLineSync()!;
+      genres.add(input);
+      if(genres.length == 6){
+        genres.remove(input);
+        print("No puede introducir mas de 6 generos");
+      }
+    }
+    if(num == 2){
+      print(genres);
+    }
+    if(num == 3){
+      break;
+    }
+  }
 
-  genres.add(input);
 
-  print(genres);
 }
