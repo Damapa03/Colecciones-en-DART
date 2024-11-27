567ef65630c37e6f389565e3ab7d977c356d55c2
Daniel Marin <damapa2003@gmail.com>
2024-11-27 17:32:01 +0100

    Ejercicio2

---
+++ Ejercicio2.dart
@@ -0,0 +1,23 @@
+﻿import 'dart:io';
+
+void main(){
+  var tareas = [];
+  while(true){
+    print("1. Introducir tarea\n2. Ver tarea\n3. Salir\n");
+    var num = int.parse(stdin.readLineSync()!);
+    if(num == 1){
+      print("¿Que genero quiere añadir?");
+      var input = stdin.readLineSync()!;
+      tareas.add(input);
+    }
+    if(num == 2){
+      for(int i = 0; i < tareas.length; i++){
+        print("${i+1}. ${tareas[i]}");
+      }
+
+    }
+    if(num == 3){
+      break;
+    }
+  }
+}
